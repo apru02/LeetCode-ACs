2b29969e125b142e30658e8413d7351e7fe8b03d
Apratim Dutta <105492094+apru02@users.noreply.github.com>
2023-09-04 21:20:21 +0530

    Time: 13 ms (78.48%) | Memory: 26.4 MB (23.70%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -11,48 +11,26 @@
  */
 class Solution {
 public:
-    // TreeNode* solve(vector<int>& preorder, vector<int>& inorder,int ind,int inorderStart,
-    //  int inorderEnd,map<int,int> &mp){
-    //      int n = inorder.size();
-    //      if(ind >= n || inorderStart > inorderEnd){
-    //          return nullptr;
-    //      }
-    //      int element = preorder[ind];
-    //      int position = mp[element];
-    //      TreeNode* node = new TreeNode(element);
-    //      ind++;
-    //      node->left = solve(preorder,inorder,ind,inorderStart,position - 1,mp);
-    //      node->right = solve(preorder,inorder,ind,position+1,inorderEnd,mp);
-    //      return node;
-    // }
-      int Findposition(vector<int>& in,int inorderStart , int inorderEnd , int element ,int  n){
-        for(int i = inorderStart ; i<=inorderEnd ;i++){
-            if(in[i]==element){
-                return i ; 
-            }
-        }
-        return -1 ;
-    }
-    TreeNode* solve(vector<int>& in, vector<int>& pre, int &index ,int inorderStart , int inorderEnd , int n){
-        if(index>=n || inorderStart>inorderEnd){
-            return NULL ;
-        }
-
-        int element = pre[index++]; 
-        TreeNode* root  = new TreeNode(element); 
-        int position  = Findposition(in , inorderStart , inorderEnd ,element, n);
-        
-        root->left = solve(in , pre , index  , inorderStart , position-1 ,n);
-        root->right = solve(in , pre , index , position+1 , inorderEnd ,n);
-        
-        return root ;
-        
+    TreeNode* solve(vector<int>& preorder, vector<int>& inorder,int &ind,int inorderStart,int inorderEnd,map<int,int> &mp){
+         int n = inorder.size();
+         if(ind >= n || inorderStart > inorderEnd){
+             return NULL;
+         }
+         int element = preorder[ind];
+         int position = mp[element];
+         TreeNode* node = new TreeNode(element);
+         ind++;
+         node->left = solve(preorder,inorder,ind,inorderStart,position - 1,mp);
+         node->right = solve(preorder,inorder,ind,position+1,inorderEnd,mp);
+         return node;
     }
     TreeNode* buildTree(vector<int>& pre, vector<int>& in) {
        
          int preorderindex  = 0 ;  
-         int n = pre.size();
-         TreeNode* ans = solve(in , pre , preorderindex , 0  , n-1 , n );
+         int n = in.size();
+         map<int,int> mp;
+         for(int i =0;i<n;i++) mp[in[i]] = i;
+         TreeNode* ans = solve(pre , in , preorderindex , 0  , n-1 , mp );
          return ans ;
     }
 };
