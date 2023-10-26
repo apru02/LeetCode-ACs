3bc0a935043186cb63ac7851194d1e7688350f63
Apratim Dutta <105492094+apru02@users.noreply.github.com>
2023-10-26 15:37:48 +0530

    Time: 182 ms (7.87%) | Memory: 9.8 MB (40.55%) - LeetSync

---
+++ 843-binary-trees-with-factors/binary-trees-with-factors.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int numFactoredBinaryTrees(vector<int>& arr) {
+        sort(arr.begin(),arr.end());
+        map<int,int> index;
+        int n = arr.size();
+        int MOD = 1e9+7;
+        for(int i = 0;i<n;i++) index[arr[i]] = i;
+        vector<long long> dp(n,1);
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<i;j++){
+                if(arr[i]%arr[j] == 0){
+                    int x = arr[i]/arr[j];
+                    if(find(arr.begin(), arr.end(), x) != arr.end()){
+                        dp[i] = (dp[i] + dp[j]*dp[index[x]])%MOD;
+                    }
+                }
+            }
+        }
+        int ans = 0;
+        for(int i = 0;i<n;i++) ans = (ans+dp[i])%MOD;
+        return ans;
+    }
+};
