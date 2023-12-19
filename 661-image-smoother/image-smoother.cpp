68f6dd595730f3f55d7ccfc553646f5f10831793
Apratim Dutta <105492094+apru02@users.noreply.github.com>
2023-12-19 16:15:09 +0530

    Time: 48 ms (30.15%) | Memory: 22.1 MB (59.38%) - LeetSync

--- 661-image-smoother/image-smoother.cpp
+++ 661-image-smoother/image-smoother.cpp
@@ -6,19 +6,18 @@ public:
         vector<vector<int>> ans(n,vector<int>(m,0));
         for(int i = 0;i<n;i++){
             for(int j = 0;j<m;j++){
-                int sum = 0;
                 int cnt = 0;
                 for(int r = -1;r<=1;r++){
                     for(int c = -1;c<=1;c++){
                         int new_r = i+r;
                         int new_c = j+c;
                         if(new_r >= 0 && new_r < n && new_c >= 0 && new_c < m){
-                            sum+=img[new_r][new_c];
+                            ans[i][j]+=img[new_r][new_c];
                             cnt++;
                         } 
                     }
                 }
-                ans[i][j] = sum/cnt;
+                ans[i][j] = ans[i][j]/cnt;
             }
         }
         return ans;
