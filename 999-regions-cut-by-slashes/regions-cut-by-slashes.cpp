ffa83b3b296cacbe853b1db341068e7986d8484b
Apratim Dutta <105492094+apru02@users.noreply.github.com>
2024-08-11 00:57:58 +0530

    Time: 7 ms (64.50%) | Memory: 15.6 MB (15.72%) - LeetSync

--- 999-regions-cut-by-slashes/regions-cut-by-slashes.cpp
+++ 999-regions-cut-by-slashes/regions-cut-by-slashes.cpp
@@ -1,63 +1,44 @@
 class Solution {
 public:
-    vector<int> parent;
-    
-    int find(int x) {
-        if (x != parent[x]) {
-            parent[x] = find(parent[x]);
-        }
-        return parent[x];
-    }
-    
-    void unite(int x, int y) {
-        int rootX = find(x);
-        int rootY = find(y);
-        if (rootX != rootY) {
-            parent[rootX] = rootY;
+    void dfs(int i, int j, vector<vector<int>>&mat, vector<vector<int>>&vis){
+        vis[i][j] = 1;
+        int n = mat.size(),m=mat[0].size();
+        int delr[] = {-1,0,1,0};
+        int delc[] = {0,1,0,-1};
+        for(int k=0; k<4; k++){
+            int nrow = i+delr[k], ncol=j+delc[k];
+            if(nrow<0 || nrow>=n || ncol<0 || ncol>=m || mat[nrow][ncol]!=0 
+            || vis[nrow][ncol]) continue;
+            dfs(nrow,ncol,mat,vis);
         }
     }
-    
     int regionsBySlashes(vector<string>& grid) {
         int n = grid.size();
-        parent.resize(4 * n * n);
-        
-        for (int i = 0; i < 4 * n * n; i++) {
-            parent[i] = i;
-        }
-        
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                int index = 4 * (i * n + j);
-                char val = grid[i][j];
-                
-                // Connect internal parts of the cell
-                if (val != '\\') {
-                    unite(index, index + 1);
-                    unite(index + 2, index + 3);
+        vector<vector<int>>mat(n*3,vector<int>(n*3,0));
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(grid[i][j]=='/'){
+                    mat[i*3][j*3+2] = 1;
+                    mat[i*3+1][j*3+1] = 1;
+                    mat[i*3+2][j*3] = 1;
+                }else if(grid[i][j]=='\\'){
+                    mat[i*3][j*3] = 1;
+                    mat[i*3+1][j*3+1] = 1;
+                    mat[i*3+2][j*3+2] = 1;
                 }
-                if (val != '/') {
-                    unite(index, index + 2);
-                    unite(index + 1, index + 3);
-                }
-                
-                // Connect the cell to its neighbors
-                if (i < n - 1) {
-                    unite(index + 3, index + 4 * n);
-                }
-                if (j < n - 1) {
-                    unite(index + 2, index + 4 + 1);
+            }
+        } // till here the correct matrix have beem constructed.
+        // Now apply dfs to find the number of different islands formed by the 0's.
+        int count = 0;
+        vector<vector<int>>vis(n*3,vector<int>(n*3,0));
+        for(int i=0; i<n*3; i++){
+            for(int j=0; j<n*3; j++){
+                if(mat[i][j]==0 && !vis[i][j]){
+                    count++;
+                    dfs(i,j,mat,vis);
                 }
             }
         }
-        
-        // Count the number of connected components
-        int regions = 0;
-        for (int i = 0; i < 4 * n * n; i++) {
-            if (find(i) == i) {
-                regions++;
-            }
-        }
-        
-        return regions;
+        return count;
     }
 };
